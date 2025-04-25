3d7a862751a531997948bc3959bef21e0a706908
ismailtaher <ismail.taher@ymail.com>
2025-04-25 14:53:00 +0500

    app setup

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Header from './lib/Header.svelte';
+</script>
+
+<main>
+  <Header />
+</main>
+
+<style>
+  main {
+    height: 100vh;
+  }
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<header>
+    <h1>
+        <span>Svelte</span> crud
+    </h1>
+</header>
+
+<style>
+    header {
+        position: sticky;
+        top: 0;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    }
+    header span {
+        color: #FF3E01;
+    }
+</style>
